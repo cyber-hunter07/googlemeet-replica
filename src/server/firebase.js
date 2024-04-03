e06d7135345a405392306274fc86a4d31d7be989
cyber-hunter07 <67088576+cyber-hunter07@users.noreply.github.com>
2024-04-03 09:56:31 -0700

    Add files via upload

---
+++ src/server/firebase.js
@@ -0,0 +1,25 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAWMtyr06Wx5aMaP8gk8PKOavW5p5v-hkQ", // Add API Key
+  databaseURL:"https://meet-appa-d7ff6-default-rtdb.asia-southeast1.firebasedatabase.app/" // Add databaseURL
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase;
+
+var firepadRef = firebase.database().ref();
+
+export const userName = prompt("What's your name?");
+const urlparams = new URLSearchParams(window.location.search);
+const roomId = urlparams.get("id");
+
+if (roomId) {
+  firepadRef = firepadRef.child(roomId);
+} else {
+  firepadRef = firepadRef.push();
+  window.history.replaceState(null, "Meet", "?id=" + firepadRef.key);
+}
+
+export default firepadRef;
